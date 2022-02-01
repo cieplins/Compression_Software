ff79d7c7842c15425e9f7a5a69bfa0d177c4bbeb
cieplins <73887754+cieplins@users.noreply.github.com>
2022-02-01 09:10:17 +0100

    crf + resolution

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-import ffmpeg
 from scenedetect import video_splitter
 from scenedetect import VideoManager
 from scenedetect import SceneManager
+import json
 from moviepy.editor import *
-from ffmpeg_quality_metrics import FfmpegQualityMetrics as ffqm
 import subprocess
+import glob
+import os
+import shutil
+import logging
+import json
 
 #Content-aware scene detection:
 from scenedetect.detectors import ContentDetector
@@ -27,151 +31,217 @@ def find_scenes(video_path, threshold=30.0):
     # Each returned scene is a tuple of the (start, end) timecode.
     return scene_manager.get_scene_list()
 
-#Compress
-def compress(conv_video, ref_video, value):
-    p = subprocess.Popen(['ffmpeg', '-y', '-i', ref_video , '-c:v', 'libx264', '-b:v', value, '-loglevel', 'quiet', '-preset', 'fast', '-pass', '1', '-an', '-f', 'mp4', 'NUL'])
+#if you want to use different output resolution you have to change it here:
+def compress_crf(conv_video, ref_video, crf_value, scale):
+    p = subprocess.Popen(['ffmpeg', '-y', '-i', ref_video, '-vf', scale, '-sws_flags', 'bilinear', 'down.mp4'])
     p.wait()
-    p = subprocess.Popen(['ffmpeg', '-y','-i', ref_video , '-c:v', 'libx264', '-b:v', value, '-loglevel', 'quiet', '-preset', 'fast', '-pass', '2', '-c:a', 'copy', conv_video])
+    p = subprocess.Popen(['ffmpeg', '-y', '-i', 'down.mp4', '-c:v', 'libx264',  '-preset', 'fast', '-crf', crf_value, '-c:a', 'copy', 'crf.mp4'])
+    p.wait()
+    p = subprocess.Popen(['ffmpeg', '-y', '-i', 'crf.mp4', '-vf', 'scale=1920x1080', '-sws_flags', 'bilinear', conv_video])
     p.wait()
     return 0
 
-#Check vmaf
-def check_vmaf(conv_video, ref_video):
-    p = subprocess.run(['ffmpeg_quality_metrics', conv_video, ref_video, '-m', 'vmaf', '--metrics', 'vmaf'], capture_output=True)
-    s = str(p)
-    start = s.find('"average":') + 11
-    end = s.find('"average":') + 15
-    current_vmaf = s[start:end]
-    return current_vmaf
+def write_json(new_data, filename):
+    with open(filename,'r+') as file:
+          # First we load existing data into a dict.
+        file_data = json.load(file)
+        # Join new_data with file_data inside emp_details
+        file_data["content"].append(new_data)
+        # Sets file's current position at offset.
+        file.seek(0)
+        # convert back to json.
+        json.dump(file_data, file, indent = 4)
 
-#Increase bitrate
-def add_br(conv_video, ref_video, br0):
-    for i in range(2):
-        br0 += i
-    br_value = "{}k".format(br0)
-    compress(conv_video, ref_video, br_value)
-    vmaf_now = check_vmaf(conv_video, ref_video)
-    print("this is vmaf_now")
-    print(vmaf_now)
-    print("This is br_value")
-    print(br_value)
-    vmaf_now_f = float(vmaf_now)
-    return vmaf_now_f
+my_input_dir = "files_to_compress"
+my_output_dir = "created_files"
+path_parent = os.getcwd()
+print(path_parent)
+working_dir = "{}/{}".format(path_parent, my_input_dir)
+results_dir = "{}/{}".format(path_parent, my_output_dir)
 
-#Compress until wanted vmaf is obtained
-def compress_scene(conv_video, ref_video, br_init_k, br_init, vmaf_wanted):
-#Compress and check vmaf for initial bitrate
-    compress(conv_video, ref_video, br_init_k)
-    vmaf_current = check_vmaf(conv_video, ref_video)
-    vmaf_current_f = float(vmaf_current)
-    vmaf_wanted_i = int(vmaf_wanted)
+os.chdir(working_dir)
+#if that's avi file convert it to mp4
+for file in glob.glob("*.avi"):
+    os.chdir(working_dir)
+    input_name = file[:len(file) - 4]
+    avi_name = "{}.avi".format(input_name)
+    video_mp4 = "{}.mp4".format(input_name)
+    p = subprocess.Popen(['ffmpeg', '-y', '-i', avi_name, '-vcodec', 'libx264', video_mp4])
+    p.wait()
 
-#If current vmaf value is not your wanted one, increase bitrate and check again
+for file in glob.glob("*.mp4"):
+    my_vmaf = int(file[0:2])
+    if my_vmaf == 99:
+        my_vmaf = 100
+    input_name = file[3:len(file) - 4]
+    video_names = '{}_$SCENE_NUMBER.mp4'.format(input_name)
+    full_video_y4m = "{}.y4m".format(input_name)
+    txt_file_pth = "{}_{}.txt".format(input_name, my_vmaf)
+    json_file_pth = "{}_{}.json".format(input_name, my_vmaf)
+    output_name = '{}_vmaf{}.mp4'.format(input_name, my_vmaf)
+    input_video_pth = [file]
+    resolution = []
+    with open(json_file_pth, 'w') as f:
+        jsondata = {}
+        jsondata['file_name'] = output_name
+        jsondata['vmaf'] = my_vmaf
+        jsondata['content'] = []
+        json.dump(jsondata, f, indent = 4)
+    if my_vmaf == 100:
+        list_crf = [16]
+        resolution.append('scale=1920x1080')
+    elif my_vmaf == 80:
+        list_crf = [16, 20, 24, 26, 28, 30, 32, 35]
+        resolution.append('scale=1280x720')
+        resolution.append('scale=854x480')
+    elif my_vmaf == 60:
+        list_crf = [16, 20, 24, 26, 28, 30, 32, 35]
+        resolution.append('scale=1280x720')
+        resolution.append('scale=854x480')
+        resolution.append('scale=640x360')
+    elif my_vmaf == 40:
+        list_crf = [16, 20, 24, 26, 28, 30, 32, 35]
+        resolution.append('scale=854x480')
+        resolution.append('scale=640x360')
+        resolution.append('scale=426x240')
+    elif my_vmaf == 20:
+        list_crf = [16, 20, 24, 26, 28, 30, 32, 35]
+        resolution.append('scale=426x240')
 
-    while vmaf_current_f <= (vmaf_wanted_i + 2):
-        if br_init >= 30:
-            if vmaf_current_f >= (vmaf_wanted_i - 2):
-                print("Done")
-                break
-            elif vmaf_current_f <= (vmaf_wanted_i - 30):
-                print("Vmaf too low (30)")
-                vmaf_current_f = add_br(conv_video, ref_video, br_init)
-                br_init += 200
-            elif vmaf_current_f <= (vmaf_wanted_i - 20):
-                print("Vmaf too low (20)")
-                vmaf_current_f = add_br(conv_video, ref_video, br_init)
-                br_init += 100
-            elif vmaf_current_f <= (vmaf_wanted_i - 10):
-                print("Vmaf too low (10)")
-                vmaf_current_f = add_br(conv_video, ref_video, br_init)
-                br_init += 50
-            else:
-                print("Vmaf too low")
-                vmaf_current_f = add_br(conv_video, ref_video, br_init)
-                br_init += 10
-        else:
-            break
+    scenes = find_scenes(file)
+    video_splitter.split_video_ffmpeg(input_video_pth, scenes, video_names, input_name,
+                                      arg_override='-c:v libx264 -preset fast -crf 0 -c:a aac',
+                                      hide_progress=False, suppress_output=False)
+    dict_crf_to_scene_vmaf = {}
+    dict_files_names = {}
+    dict_crfs = {}
+    dict_resolutions = {}
+    for r in range(len(resolution)):
+        for q in range(len(list_crf)):
+            full_video_compressed = "{}_crf{}_{}.mp4".format(input_name, list_crf[q], resolution[r])
+            compress_crf(full_video_compressed, file, str(list_crf[q]), resolution[r])
+            input_name_c = '{}_crf{}_{}'.format(input_name, list_crf[q], resolution[r])
+            video_names_c = '{}_$SCENE_NUMBER.mp4'.format(input_name_c)
+            input_video_pth2 = [full_video_compressed]
+            video_splitter.split_video_ffmpeg(input_video_pth2, scenes, video_names_c, input_name_c,
+                                              arg_override='-c:v libx264 -preset fast -crf 0 -c:a aac',
+                                              hide_progress=False, suppress_output=False)
+            dict_crf_to_scene_vmaf[r, q] = []
+            dict_files_names[r, q] = []
+            dict_crfs[r, q] = []
+            dict_resolutions[r, q] = []
+            scene_n = 1
+            for i in range(len(scenes)):
+                if i < 9:
+                    current_scene = '{}_00{}.mp4'.format(input_name, scene_n)
+                    current_scene_c = '{}_00{}.mp4'.format(input_name_c, scene_n)
+                    current_scene_y4m = '{}_00{}.y4m'.format(input_name, scene_n)
+                    current_scene_c_y4m = '{}_00{}.y4m'.format(input_name_c, scene_n)
+                elif i < 99:
+                    current_scene = '{}_0{}.mp4'.format(input_name, scene_n)
+                    current_scene_c = '{}_0{}.mp4'.format(input_name_c, scene_n)
+                    current_scene_y4m = '{}_0{}.y4m'.format(input_name, scene_n)
+                    current_scene_c_y4m = '{}_0{}.y4m'.format(input_name_c, scene_n)
+                else:
+                    current_scene = '{}_{}.mp4'.format(input_name, scene_n)
+                    current_scene_c = '{}_{}.mp4'.format(input_name_c, scene_n)
+                    current_scene_y4m = '{}_{}.y4m'.format(input_name, scene_n)
+                    current_scene_c_y4m = '{}_{}.y4m'.format(input_name_c, scene_n)
 
+                p = subprocess.Popen(
+                    ['ffmpeg', '-y', '-i', current_scene, '-pix_fmt', 'yuv420p', '-vsync', '1', current_scene_y4m])
+                p.wait()
+                p = subprocess.Popen(
+                    ['ffmpeg', '-y', '-i', current_scene_c, '-pix_fmt', 'yuv420p', '-vsync', '1', current_scene_c_y4m])
+                p.wait()
+                p = subprocess.run([
+                    'vmaf',
+                    '--reference', current_scene_y4m,
+                    '--distorted', current_scene_c_y4m,
+                    '--model', 'path=/Users/nataliacieplinska/Downloads/vmaf-master/model/vmaf_v0.6.1.json',
+                    '--threads', '8',
+                    '--output', '/dev/stdout', '--json'
+                ], capture_output=True)
+                print(json.loads(p.stdout)['pooled_metrics']['vmaf']['mean'])
+                vmaf_for_scene = float(json.loads(p.stdout)['pooled_metrics']['vmaf']['mean'])
+                dict_crf_to_scene_vmaf[r, q].append(vmaf_for_scene)
+                dict_files_names[r, q].append(current_scene_c)
+                dict_crfs[r, q].append(list_crf[q])
+                dict_resolutions[r, q].append(resolution[r])
+                scene_n += 1
+                # delete y4m files, so they don't take up too much space
+                p = subprocess.Popen(['rm', current_scene_y4m])
+                p.wait()
+                p = subprocess.Popen(['rm', current_scene_c_y4m])
+                p.wait()
+            print(dict_crf_to_scene_vmaf)
+            print(dict_files_names)
 
-    while vmaf_current_f >= (vmaf_wanted_i - 2):
-        if br_init >= 30:
-            if vmaf_current_f <= (vmaf_wanted_i + 2):
-                print("Done")
-                break
-            elif vmaf_current_f >= (vmaf_wanted_i + 30):
-                print("Vmaf too high (30)")
-                vmaf_current_f = add_br(conv_video, ref_video, br_init)
-                br_init -= 200
-            elif vmaf_current_f >= (vmaf_wanted_i + 20):
-                print("Vmaf too high (20)")
-                vmaf_current_f = add_br(conv_video, ref_video, br_init)
-                br_init -= 100
-            elif vmaf_current_f >= (vmaf_wanted_i + 10):
-                print("Vmaf too high (10)")
-                vmaf_current_f = add_br(conv_video, ref_video, br_init)
-                br_init -= 50
-            else:
-                print("Vmaf too high")
-                vmaf_current_f = add_br(conv_video, ref_video, br_init)
-                br_init -= 10
-        else:
-            break
+    list_clips = []
+    min_scene_vmafs = []
+    jsondata = {}
+    file_name = {}
+    file_vmaf = {}
+    content = {}
 
-#Here put your initial values of bitrate and your wanted vmaf value
-br_init_k = '1000k'
-br_init = 999
-#This part is to be changed accordingly to needs
-vmaf_wanted = '60'
-input_name = 'you_1080'
-full_video_name = "you_1080.mp4"
-compressed_name = "you_1080_compressed.mp4"
+    for scene_number in range(len(scenes)):
+        sc = scene_number + 1
+        scene_vmafs = []
+        scene_names = []
+        scene_crfs = []
+        scene_res = []
+        for qp, qp_scene_vmafs in dict_crf_to_scene_vmaf.items():
+            scene_vmafs.append(qp_scene_vmafs[scene_number])
+        for name, name_scenes in dict_files_names.items():
+            scene_names.append(name_scenes[scene_number])
+        for res, res_scenes in dict_resolutions.items():
+            scene_res.append(res_scenes[scene_number])
+        for crf, crf_scenes in dict_crfs.items():
+            scene_crfs.append(crf_scenes[scene_number])
+        calculated_min = min(range(len(scene_vmafs)), key=lambda index: abs(scene_vmafs[index] - my_vmaf))
+        min_scene_vmafs.append(calculated_min)
+        print(scene_vmafs)
+        print(scene_names)
+        picked_vmaf = scene_vmafs[calculated_min]
+        picked_scene = scene_names[calculated_min]
+        picked_crf = scene_crfs[calculated_min]
+        picked_res = scene_res[calculated_min]
+        print(picked_vmaf)
+        print(picked_scene)
+        print(picked_crf)
+        print(picked_res)
+        y = {"scene_number": sc,
+             "calculated_vmaf" : picked_vmaf,
+             "resolution": picked_res[6:len(str(picked_res))],
+             "crf": picked_crf}
+        write_json(y, json_file_pth)
 
-input_video_pth = [full_video_name]
-scenes = find_scenes(full_video_name)
-video_splitter.split_video_ffmpeg(input_video_pth, scenes, 'you_1080_$SCENE_NUMBER.mp4', input_name, arg_override='-c:v libx264 -preset fast -crf 21 -c:a aac', hide_progress=False, suppress_output=False)
-
-vmaf_wanted_i = int(vmaf_wanted)
-scene_n = 1
-list_clips = []
-for i in range(len(scenes)):
-    if i < 9:
-        con_v = "conv_00{}.mp4".format(scene_n)
-        ref_v = "%s_00{}.mp4".format(scene_n) % input_name
-        print(ref_v)
-        compress_scene(con_v, ref_v, br_init_k, br_init, vmaf_wanted)
-        clip = VideoFileClip(con_v)
+        clip = VideoFileClip(picked_scene)
         duration = clip.duration
-        clip_n = "clip{}".format(scene_n)
-        print(clip_n)
+        clip_n = "clip{}".format(scene_number)
         clip_n = clip.subclip(0, duration)
         list_clips.append(clip_n)
-        scene_n += 1
-    elif i < 100:
-        con_v = "conv_0{}.mp4".format(scene_n)
-        ref_v = "%s_0{}.mp4".format(scene_n) % input_name
-        print(ref_v)
-        compress_scene(con_v, ref_v, br_init_k, br_init, vmaf_wanted)
-        clip = VideoFileClip(con_v)
-        duration = clip.duration
-        clip_n = "clip{}".format(scene_n)
-        print(clip_n)
-        clip_n = clip.subclip(0, duration)
-        list_clips.append(clip_n)
-        scene_n += 1
-    else:
-        con_v = "conv_{}.mp4".format(scene_n)
-        ref_v = "%s_{}.mp4".format(scene_n) % input_name
-        print(ref_v)
-        compress_scene(con_v, ref_v, br_init_k, br_init, vmaf_wanted)
-        clip = VideoFileClip(con_v)
-        duration = clip.duration
-        clip_n = "clip{}".format(scene_n)
-        print(clip_n)
-        clip_n = clip.subclip(0, duration)
-        list_clips.append(clip_n)
-        scene_n += 1
 
 
-print(list_clips)
-final = concatenate_videoclips(list_clips)
-final.write_videofile(compressed_name)
+    final = concatenate_videoclips(list_clips)
+    try:
+        final.write_videofile(output_name, threads=6, logger=None)
+        logging.info("Saved .mp4 without Exception at {}".format(output_name))
+    except IndexError:
+        print("saved, with exception")
+        pass
+    except Exception as e:
+        logging.warning("Exception {} was raised!!".format(e))
+        print("not saved")
+    input_file = "{}/{}/{}".format(path_parent, my_input_dir, file)
+    output_file = "{}/{}/{}".format(path_parent, my_input_dir, output_name)
+    json_file = "{}/{}/{}".format(path_parent, my_input_dir, json_file_pth)
+    files_to_copy = [input_file, json_file, output_file]
+    for f in files_to_copy:
+        shutil.copy(f, results_dir)
+
+#remove files from files_to_compress
+
+for file in os.scandir(working_dir):
+    os.remove(file.path)
+
